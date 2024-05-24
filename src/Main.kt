d19fc5abd3e1c9c689d7b53e4d91426bceab66b4
Novjean J Kannathara <novjeanj@gmail.com>
2024-05-24 21:01:35 +0530

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("HackerRank. lets do this!")
+    stdinStdout2()
+}
+
+fun stdinStdout2() {
+    val scanner = Scanner(System.`in`)
+    val i = scanner.nextInt()
+    val d = scanner.nextDouble()
+    scanner.nextLine()
+    val s = scanner.nextLine()
+
+    println("String: " + s)
+    println("Double: " + d)
+    println("Int: " + i)
+
+}
