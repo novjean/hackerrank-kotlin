aa35ff68a26122e105bb8cfe67408d6be3f026df
Novjean J Kannathara <novjeanj@gmail.com>
2024-05-24 21:52:18 +0530

    output formatting

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,19 @@ import java.util.*
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println("HackerRank. lets do this!")
-    stdinStdout2()
+//    stdinStdout2()
+    outputFormatting()
+}
+
+fun outputFormatting() {
+    val sc = Scanner(System.`in`)
+
+    for (i in 1..3){
+        val s1 = sc.next()
+        val x = sc.nextInt()
+
+        println(String.format("%-15s%03d", s1, x))
+    }
 }
 
 fun stdinStdout2() {
